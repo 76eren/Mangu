4ac9d793d4706ec50540e542c9f9210b89d00e73
AHsofty <e.basaran2004@gmail.com>
2023-12-17 00:51:40 +0100

    Fix: Fixed an issue where the app would sometimes crash when loading the home screen.
    I was unable to re-create this error however this should fix it.

--- app/src/main/java/com/example/mangareader/Activities/HomeActivity.java
+++ app/src/main/java/com/example/mangareader/Activities/HomeActivity.java
@@ -24,6 +24,7 @@ import com.google.android.material.navigation.NavigationView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -91,24 +92,22 @@ public class HomeActivity extends AppCompatActivity {
             }
 
             if (homeData != null) {
-                ArrayList<HomeMangaClass> latest = homeData.get("latest");
+                CopyOnWriteArrayList<HomeMangaClass> latest = new CopyOnWriteArrayList<>(homeData.get("latest"));
 
                 // Does the latest
-                if (latest != null) {
-                    // We first start with the latest
-                    List<RviewAdapterHome.Data> data = new ArrayList<>();
-                    for (HomeMangaClass i : latest) {
-                        data.add(new RviewAdapterHome.Data(context, i));
-                    }
-
-                    runOnUiThread(() -> {
-                        RecyclerView recyclerView = findViewById(R.id.RecyclerViewHomeLatest);
-
-                        RviewAdapterHome adapter = new RviewAdapterHome(context, data, "imageview");
-                        recyclerView.setAdapter(adapter);
-
-                    });
+                // We first start with the latest
+                List<RviewAdapterHome.Data> data = new ArrayList<>();
+                for (HomeMangaClass i : latest) {
+                    data.add(new RviewAdapterHome.Data(context, i));
                 }
+
+                runOnUiThread(() -> {
+                    RecyclerView recyclerView = findViewById(R.id.RecyclerViewHomeLatest);
+
+                    RviewAdapterHome adapter = new RviewAdapterHome(context, data, "imageview");
+                    recyclerView.setAdapter(adapter);
+
+                });
 
                 // Does the popular
                 ArrayList<HomeMangaClass> popular = homeData.get("popular");
